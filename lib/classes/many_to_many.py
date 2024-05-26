07740f01f84a4765ac74f81b5536c7d51fe308a6
victoria <kariukivictoria428@gmail.com>
2024-05-27 00:13:21 +0300

    commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -4,21 +4,41 @@ class Article:
         self.magazine = magazine
         self.title = title
         
+def Author(self):
+    print("John")
+    
+def Magazine(self):
+    print("The Gen Z")
+
+p1 = Article("John,The Gen Z,Hobbies")
+p1.author()
+p1.magazine()
+
+print(p1.Article)
+        
 class Author:
     def __init__(self, name):
         self.name = name
-
+    
     def articles(self):
-        pass
+        print("Hobbies","Fun","Food","Music","Education","Lifestyle","Dress code.")
+        return  
 
     def magazines(self):
-        pass
+        print("The Gen Z","Fashion","Politics","Lifestyle")
+        return
 
     def add_article(self, magazine, title):
-        pass
+        self.magazine = magazine
+        self.title = title
 
     def topic_areas(self):
-        pass
+        print("listed topics.")
+        return
+
+p1 = Author("John")
+
+print(p1.name),
 
 class Magazine:
     def __init__(self, name, category):
@@ -26,13 +46,22 @@ class Magazine:
         self.category = category
 
     def articles(self):
-        pass
+        print("Hobbies","Fun","Food","Music","Education","Lifestyle","Dress code")
+        return
+        
 
     def contributors(self):
-        pass
+        print("John,Aquilla,Mercy")
+        return
 
     def article_titles(self):
-        pass
+        print("Hobbies,Fun")
+        return
 
     def contributing_authors(self):
-        pass
+        print("John")
+        return
+    
+p1 = Magazine("The Gen Z,Teen one")
+
+print (p1.Magazine)
